35918cf4031fe975258e1f51ca8a83b832bb5134
diamond-nicholas <diamondnicholas154@gmail.com>
2021-11-14 06:48:13 +0100

    add basic style and arrangement to newtip component

--- src/Components/NewTips.js
+++ src/Components/NewTips.js
@@ -1,33 +1,36 @@
 import React from 'react';
+import('../Styles/NewTips.css');
 
 const NewTips = () => {
   return (
     <div>
-      <article>
+      <article className='tips-header'>
         <h3>Free Tips For Today</h3>
         <button>14, Nov 2021 </button>
       </article>
-      <table>
-        <tr>
-          <th>Time</th>
-          <th>League</th>
-          <th>Match</th>
-          <th>Tips</th>
-        </tr>
-        <tr>
-          <td>22:30</td>
-          <td>Chile</td>
-          <td>Curico Unido vs Colo Colo</td>
-          <td>X2</td>
-        </tr>
-        <tr>
-          <td>22:30</td>
-          <td>Chile</td>
-          <td>Curico Unido vs Colo Colo</td>
-          <td>X2</td>
-        </tr>
-      </table>
-      <aside>
+      <div className='fixtures'>
+        <table>
+          <tr>
+            <th>Time</th>
+            <th>League</th>
+            <th>Match</th>
+            <th>Tips</th>
+          </tr>
+          <tr>
+            <td>22:30</td>
+            <td>Chile</td>
+            <td>Curico Unido vs Colo Colo</td>
+            <td>X2</td>
+          </tr>
+          <tr>
+            <td>22:30</td>
+            <td>Chile</td>
+            <td>Curico Unido vs Colo Colo</td>
+            <td>X2</td>
+          </tr>
+        </table>
+      </div>
+      <aside className='urls'>
         <button>View More</button>
         <button>Banker Tip of the Day</button>
       </aside>
