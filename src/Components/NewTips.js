62950d6766e0d3a6b2570a723a52bab14fd1d02e
diamond-nicholas <diamondnicholas154@gmail.com>
2021-11-14 06:37:47 +0100

    add structure for newtips component

--- src/Components/NewTips.js
+++ src/Components/NewTips.js
@@ -3,7 +3,34 @@ import React from 'react';
 const NewTips = () => {
   return (
     <div>
-      <h1>this takes new tips for the day</h1>
+      <article>
+        <h3>Free Tips For Today</h3>
+        <button>14, Nov 2021 </button>
+      </article>
+      <table>
+        <tr>
+          <th>Time</th>
+          <th>League</th>
+          <th>Match</th>
+          <th>Tips</th>
+        </tr>
+        <tr>
+          <td>22:30</td>
+          <td>Chile</td>
+          <td>Curico Unido vs Colo Colo</td>
+          <td>X2</td>
+        </tr>
+        <tr>
+          <td>22:30</td>
+          <td>Chile</td>
+          <td>Curico Unido vs Colo Colo</td>
+          <td>X2</td>
+        </tr>
+      </table>
+      <aside>
+        <button>View More</button>
+        <button>Banker Tip of the Day</button>
+      </aside>
     </div>
   );
 };
