b8aa6343e7ae54ef5759b5a3f341df7d1add9d31
diamond-nicholas <diamondnicholas154@gmail.com>
2021-11-14 06:20:11 +0100

    add styles to nav

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,22 +1,23 @@
 import React from 'react';
-
+import('../Styles/Nav.css');
 const Nav = () => {
   return (
-    <div>
+    <div className='main'>
       <nav className='nav'>
         <header className='left'>
           {/* <img src='' alt='' /> */}
           RealFootballTips
         </header>
-        <header className='right'>
+        <header className='middle'>
           <ul>
             <li className='list-item'>Leagues</li>
             <li className='list-item'>Tips</li>
+            <li className='list-item'>Weekend 10odds</li>
             <li className='list-item'>Faqs</li>
             <li className='list-item'>Contact Us</li>
-            <li className='list-item'>Welcome, User.name</li>
           </ul>
         </header>
+        <header className='right'>Welcome, user.name</header>
       </nav>
     </div>
   );
