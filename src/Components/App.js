7088787a33efb0f35e9a37e52b8d5456fd8ca005
diamond-nicholas <diamondnicholas154@gmail.com>
2021-11-14 06:22:17 +0100

    add newtips component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,13 @@
 import Nav from './Nav';
+import NewTips from './NewTips';
+
 import('../Styles/App.css');
 
 function App() {
   return (
     <div className='App'>
       <Nav />
+      <NewTips />
     </div>
   );
 }
---
+++ src/Components/NewTips.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewTips = () => {
+  return (
+    <div>
+      <h1>this takes new tips for the day</h1>
+    </div>
+  );
+};
+
+export default NewTips;
