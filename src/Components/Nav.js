1dc0db949f1931f2c64b000ec80c597d80229492
diamond-nicholas <diamondnicholas154@gmail.com>
2021-11-14 05:47:51 +0100

    add basic navbar structure

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,7 +3,21 @@ import React from 'react';
 const Nav = () => {
   return (
     <div>
-      <h1>this is the nav bar for the tips</h1>
+      <nav className='nav'>
+        <header className='left'>
+          {/* <img src='' alt='' /> */}
+          RealFootballTips
+        </header>
+        <header className='right'>
+          <ul>
+            <li className='list-item'>Leagues</li>
+            <li className='list-item'>Tips</li>
+            <li className='list-item'>Faqs</li>
+            <li className='list-item'>Contact Us</li>
+            <li className='list-item'>Welcome, User.name</li>
+          </ul>
+        </header>
+      </nav>
     </div>
   );
 };
