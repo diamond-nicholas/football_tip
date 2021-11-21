ad915dd00dd8861a636afb44f741422e69f14fe7
diamond-nicholas <diamondnicholas154@gmail.com>
2021-11-21 08:47:19 +0100

    add tips score

---
+++ src/Components/Tips.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Tips = () => {
+ return (
+   <>
+     <main>
+       <article>
+         <h3>Tips Store</h3>
+       </article>
+       
+     </main>
+   </>
+ );
+}
+
+export default Tips
