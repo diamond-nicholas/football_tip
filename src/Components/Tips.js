4088134af8447e0dcdf5c9094733134790b2df2e
diamond-nicholas <diamondnicholas154@gmail.com>
2021-11-21 08:52:20 +0100

    add structure for tips component

--- src/Components/Tips.js
+++ src/Components/Tips.js
@@ -1,16 +1,40 @@
-import React from 'react'
+import React from 'react';
+import('../Styles/Tips.css');
 
 const Tips = () => {
- return (
-   <>
-     <main>
-       <article>
-         <h3>Tips Store</h3>
-       </article>
-       
-     </main>
-   </>
- );
-}
+  return (
+    <>
+      <main>
+        <article>
+          <h3>Tips Store</h3>
+        </article>
+        <aside>
+          <h3>Double Chance</h3>
+        </aside>
+        <aside>
+          <h3>Double Chance</h3>
+        </aside>
+        <aside>
+          <h3>Double Chance</h3>
+        </aside>
+        <aside>
+          <h3>Double Chance</h3>
+        </aside>
+        <aside>
+          <h3>Double Chance</h3>
+        </aside>
+        <aside>
+          <h3>Double Chance</h3>
+        </aside>
+        <aside>
+          <h3>Double Chance</h3>
+        </aside>
+        <aside>
+          <h3>Double Chance</h3>
+        </aside>
+      </main>
+    </>
+  );
+};
 
-export default Tips
+export default Tips;
