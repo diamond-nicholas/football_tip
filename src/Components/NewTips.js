fd285354aebc87739fa4a961b9ca9a3dd770b44b
diamond-nicholas <diamondnicholas154@gmail.com>
2021-11-21 10:08:06 +0100

    add funtionality go get the exact date

--- src/Components/NewTips.js
+++ src/Components/NewTips.js
@@ -1,12 +1,28 @@
+/* eslint-disable no-array-constructor */
 import React from 'react';
 import('../Styles/NewTips.css');
 
 const NewTips = () => {
+  const date = new Date();
+  const month = new Array();
+  month[0] = 'January';
+  month[1] = 'February';
+  month[2] = 'March';
+  month[3] = 'April';
+  month[4] = 'May';
+  month[5] = 'June';
+  month[6] = 'July';
+  month[7] = 'August';
+  month[8] = 'September';
+  month[9] = 'October';
+  month[10] = 'November';
+  month[11] = 'December';
+  var time =  `${date.getDate()}, ${month[date.getMonth()]} Today`
   return (
     <div>
       <article className='tips-header'>
         <h3>Free Tips For Today</h3>
-        <button>14, Nov 2021 </button>
+        <button>{time } </button>
       </article>
       <div className='fixtures'>
         <table>
