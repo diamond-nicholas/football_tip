e524d17a3b9f4939f56adb76175d0744b41c1abe
diamond-nicholas <diamondnicholas154@gmail.com>
2021-11-25 12:04:36 +0100

    add post api for form

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -7,16 +7,29 @@ const Admin = () => {
   const [starttime, setStartTime] = useState('');
   const [league, setLeague] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTip = { home, away, pred, starttime, league };
+    fetch('https://footballoddsapi.herokuapp.com/api/v1/tasks', {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newTip),
+    }).then(() => {
+      console.log('new tips added');
+    });
+  };
   return (
     <div>
       <h1>hello admin</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type='text'
           className='home'
           placeholder='home'
           value={home}
           onChange={(e) => setHome(e.target.value)}
+          required
         />
         <br />
         <input
@@ -25,6 +38,7 @@ const Admin = () => {
           placeholder='away'
           value={away}
           onChange={(e) => setAway(e.target.value)}
+          required
         />
         <br />
         <input
@@ -33,6 +47,7 @@ const Admin = () => {
           placeholder='predictions'
           value={pred}
           onChange={(e) => setPred(e.target.value)}
+          required
         />
         <br />
         <input
@@ -41,6 +56,7 @@ const Admin = () => {
           placeholder='league'
           value={league}
           onChange={(e) => setLeague(e.target.value)}
+          required
         />
         <br />
         <input
@@ -49,9 +65,10 @@ const Admin = () => {
           placeholder='starttime'
           value={starttime}
           onChange={(e) => setStartTime(e.target.value)}
+          required
         />
         <br />
-        <button type='submit'>Submit predictions</button>
+        <button type='submit'>Submit Tips</button>
       </form>
     </div>
   );
