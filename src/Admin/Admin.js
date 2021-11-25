b41dc473c1d44a8234ab0e86a5bbdc80ec505667
diamond-nicholas <diamondnicholas154@gmail.com>
2021-11-25 11:15:07 +0100

    create state for admin home

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Admin = () => {
+  const [home, setHome] = useState('');
   return (
     <div>
       <h1>hello admin</h1>
+      <form>
+        <input
+          type='text'
+          className='home'
+          placeholder='home'
+          value={home}
+          onChange={(e) => setHome(e.target.value)}
+        />
+        <br />
+        <input type='text' className='home' placeholder='away' />
+        <br />
+        <input type='text' className='home' placeholder='predictions' />
+        <br />
+        <input type='text' className='home' placeholder='league' />
+        <br />
+        <input type='text' className='home' placeholder='starttime' /> <br />
+        <button type='submit'>Submit predictions</button>
+      </form>
     </div>
   );
 };
