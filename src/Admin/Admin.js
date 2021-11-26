28cf5cd5664c227fa7adb03b50ee6509771b7825
diamond-nicholas <diamondnicholas154@gmail.com>
2021-11-26 03:33:09 +0100

    configure contact field structure

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,23 +1,30 @@
+// import axios from 'axios';
 import React, { useState } from 'react';
 
 const Admin = () => {
   const [home, setHome] = useState('');
   const [away, setAway] = useState('');
   const [pred, setPred] = useState('');
-  const [starttime, setStartTime] = useState('');
+  const [start_time, setStartTime] = useState('');
   const [league, setLeague] = useState('');
+  // const varlocal = 'http://127.0.0.1:3001/api/v1/tasks'
+  // const varServer = 'https://footballoddsapi.herokuapp.com/api/v1/tasks';
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newTip = { home, away, pred, starttime, league };
-    fetch('https://footballoddsapi.herokuapp.com/api/v1/tasks', {
-      method: 'POST',
-      mode: 'no-cors',
-      headers: { 'Content-Type': 'application/json' },
+    const newTip = { home, away, pred, start_time, league };
+    fetch('http://127.0.0.1:3001/api/v1/tasks', {
       body: JSON.stringify(newTip),
-    }).then(() => {
-      console.log('new tips added');
-    });
+      headers: { 'Content-Type': 'application/json' },
+      mode: 'no-cors',
+      method: 'POST',
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .then((error) => {
+        console.log(error);
+      });
   };
   return (
     <div>
@@ -63,7 +70,7 @@ const Admin = () => {
           type='text'
           className='home'
           placeholder='starttime'
-          value={starttime}
+          value={start_time}
           onChange={(e) => setStartTime(e.target.value)}
           required
         />
--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -3,7 +3,35 @@ import React from 'react';
 const Contact = () => {
   return (
     <div>
-      <h1>hello worlds</h1>
+      <h3>Contact Us</h3>
+      <p>How may we be of help to you?</p>
+      <p>
+        Send us a message by using the form below and a team member will get
+        back to you as soon as possible.
+      </p>
+      <article>
+        <form>
+          <label>Name:</label>
+          <input type='text' />
+          <label>Email:</label>
+          <input type='email' />
+          <label>Phone Number:</label>
+          <input type='text' />
+        </form>
+        <div>
+          <p>
+            For general enquiries, please reach us hello@freefootballodds.com
+          </p>
+          <p>
+            For Advert & sponsorship please reach us advert@freefootballodds.com
+          </p>
+          <p>
+            Call us on +234 813 1149 662, +254 796 118 357 (Kenya), +233 55 781
+            2137 (Ghana) Working Hours - Mondays to Fridays (9am to 5pm)
+            Saturdays (10am-12noon)
+          </p>
+        </div>
+      </article>
     </div>
   );
 };
